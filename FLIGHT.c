356d78272f35376364e286848e0dd5065a095bb5
Rakshak-D <rakshakmce@gmail.com>
2024-12-21 17:04:44 +0530

    Add files via upload

--- FLIGHT.c
+++ FLIGHT.c
@@ -2,6 +2,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include <string.h>
+int countuser=10;
 //creating a structure to store flight details
 struct Flight 
 {
@@ -18,53 +19,34 @@ struct city
 {
     char cities[20]; 
 }city_list[7];
+struct userdetails
+{
+    char username[20];
+    char email[20];
+    char password[20];
+}user[20];
 //Creating prototypes for function
+void userdata();
+void files();
+void createnewaccount();
+void login();  
+void userlogin();
 void displayallflights();
 void displaysource();    
 void displaysourcetodesination();   
+int check();
 //Main function
 int main()
 {
-    FILE *fp, *fp2;
-    int flightn=20,i=0,choice;
-    //opening FLIGHT DETAILS csv file
-    fp = fopen("FLIGHTDETAILS.csv", "r");
-    if (fp == NULL) 
-    {
-        printf("Error opening file FLIGHT DETAILS");
-        return 1;
-    }
-    //opening LOCATION csv file
-    fp2 = fopen("PLACES.csv", "r");
-    if (fp2 == NULL) 
-    {
-        printf("Error opening file PLACES");
-        return 2;
-    }
-    //reading flight details from csv file
-    char line[60];
-    while (fgets(line, sizeof(line), fp)){
-        if(sscanf(line, "%[^,],%[^,],%[^,],%[^,],%[^,],%f,%f", &flights[i].flightID, &flights[i].source, &flights[i].arrivalTime, &flights[i].destination, &flights[i].departureTime, &flights[i].flightTime, &flights[i].fare) == 7) {
-            i++;
-            if (i==20) {
-            break;
-            }
-        }
-    }
-    i=0;
-    //reading city details from csv file
-    while (fscanf(fp2, "%s", city_list[i].cities) == 1) 
-    {
-        i++;
-        if (i == 7) 
-        {
-            break;
-        }
-    }
-    //closing files
-    fclose(fp);
-    fclose(fp2);
+    int flightn=20,choice;
+    //fetching all files
+    files();
+    userdata();
+    //user login
+    userlogin();
+
     //Asking user to select option
+    
     do
     {
         printf("Select a option\n");
@@ -92,6 +74,71 @@ int main()
         }
     } while (choice!=4);
     return 0;   
+}
+void files()
+{
+    FILE *fp,*fp2;
+    int i=0;
+    //opening FLIGHT DETAILS csv file
+    fp = fopen("FLIGHTDETAILS.csv", "r");
+    if (fp == NULL) 
+    {
+        printf("Error opening file FLIGHT DETAILS");
+        return ;
+    }
+    //opening LOCATION csv file
+    fp2 = fopen("PLACES.csv", "r");
+    if (fp2 == NULL) 
+    {
+        printf("Error opening file PLACES");
+        return ;
+    }
+    
+    //reading flight details from csv file
+    char line1[60];
+    while (fgets(line1, sizeof(line1), fp)){
+        if(sscanf(line1, "%[^,],%[^,],%[^,],%[^,],%[^,],%f,%f", &flights[i].flightID, &flights[i].source, &flights[i].arrivalTime, &flights[i].destination, &flights[i].departureTime, &flights[i].flightTime, &flights[i].fare) == 7) 
+        {
+            i++;
+        }
+    }
+    i=0;
+    char line2[60];
+    //reading city details from csv file
+     while (fscanf(fp2, "%s", city_list[i].cities) == 1) 
+    {
+        i++;
+        if (i == 7) 
+        {
+            break;
+        }
+    } 
+    //closing files
+    fclose(fp);
+    fclose(fp2);
+}
+void userdata()
+{
+    FILE *fp3;   
+    fp3 = fopen("PLACES.csv", "r");
+    if (fp3 == NULL) 
+    {
+        printf("Error opening user files");
+        return ;
+    } 
+    int i=0;
+    //reading user details
+    char line3[60];
+    while (fgets(line3, sizeof(line3), fp3))
+    {
+        if(sscanf(line3, "%[^,],%[^,],%[^,]", user[i].username,&user[i].email,&user[i].password)==3)
+         {
+            i++;
+            countuser=i;
+        }
+    }
+    //closing file
+    fclose(fp3);
 }
 //function to display all flights
 void displayallflights()
@@ -279,5 +326,151 @@ void displaysourcetodesination()
     }      
     printf("\n");
 }
+void userlogin()
+{
+    char option;
+    for(int j=0;j<50;j++)
+        {
+            printf("-");
+        }
+    printf("\n");
+    printf("%40s","Welcome to Flight Finder\n");
+    printf("%5s","Do you have a travel account with us? [Y/N]\n");
+    scanf("%c",&option);
+    do
+    {
+            if (option == 'y' || option == 'Y') 
+            {
+                login();    
+                return;               
+            }
+            else if(option == 'n'|| option == 'N')
+            {
+                createnewaccount();
+                return;
+            }
+            else
+            {
+                printf("\nERROR: Please type [y/Y] or [n/N]: ");
+            }    
+    }while(1);     
 
+}
+void login()
+{
+    char name[20],password[20],option;
+    int error,i=-1;
+    here:
+    if(i==2)
+    {
+        printf("10%s","All attempts are over :(\n");
+        printf("Do you want to create new account\n");
+        scanf("%c",&option);
+        do
+        {
+            if (option == 'y' || option == 'Y') 
+            {
+                createnewaccount();    
+                return;               
+            }
+            else if(option == 'n'|| option == 'N')
+            {
+                printf("10%s","Account signin Failed :(");
+                    exit(0);
+            }
+            else
+            {
+                printf("\nERROR: Please type [y/Y] or [n/N]: \n");
+            }    
+        }while(1);     
+        
+    }
+    i++;
+    if(i==0)
+    {
+        printf("10%s","Please type the following details\n");
+    }
+    else
+    {
+    printf("Attempt %d\n",i);
+    }
+    printf("%s","Enter your username or email address.\n");
+    scanf("%s",name);
+    here1:
+    printf("%s","Enter your password\n");
+    scanf("%s",password);
+    error=check(name,password);
+    if(error==1)
+    {
+        goto here1;
+    }
+    else if(error==2)
+    {
+        goto here;
+    }
+    for(int j=0;j<50;j++)
+    {
+        printf("-");
+    }
+    printf("\n");
+} 
+int check(char name[20],char password[20])
+{
+    for(int i=0;i<countuser;i++)
+    {
+        //checking if given username or email is valid or not
+        if( strcmp(name,user[i].username)==0||strcmp(name,user[i].email))
+        {
+            if(strcmp(password,user[i].password)==0)
+            {
+                printf("%10s","Welcome Back!!  ");
+                printf("s\n",user[i].username);
+                return 0;
+            }
+            else
+            {
+                printf("Wrong password!! Please try again\n");
+                return 1;
+            }
+        }
+        else
+        {
+            printf("10%s","Please Enter valid username");
+            return 2;
+        }
+    }
+}    
+void createnewaccount()
+{
+    char username[20],email[20],password[20];
+    FILE *fp;
+    fp = fopen("userdetails.csv", "a");
+    if (fp == NULL) 
+    {
+        printf("Error opening file.\n");
+        return ;
+    }
+    for(int i=0;i<50;i++)
+        {
+            printf("-");
+        }
+    printf("\n\n%10s","Lets create your new account :)\n");
+    //asking user to enter details for their new account
+    printf("%10s","Enter Your Username\n");
+    scanf("%s",&username);
+    printf("%10s","Enter your Email Adress\n");
+    scanf("%s",&email);
+    printf("%10s","Enter your password\n");
+    scanf("%s",&password);
+    fprintf(fp, "%s,%s,%s\n", username, email, password);
+    for(int j=0;j<50;j++)
+    {
+        printf("-");
+    }
+    // Close the file
+    fclose(fp);
+    userdata();
+    printf("\n%10s%s\n","Welcome!! ",username);
+    printf("10%s","How may i help you\n");
+}
     
