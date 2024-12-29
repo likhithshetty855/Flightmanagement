bd8ec78b5fa206c974845fb909b98f41c28d3857
Rakshak-D <rakshakmce@gmail.com>
2024-12-29 20:21:35 +0530

    Adding a feature to display cheapest flight according to fare

--- FLIGHT.c
+++ FLIGHT.c
@@ -6,7 +6,7 @@ int countuser=0;
 int countflight=0;
 int countplaces=0;
 //creating a structure to store flight details
-struct Flight 
+struct Flight
 {
     char flightID[10];
     char source[20];
@@ -19,7 +19,7 @@ struct Flight
 //creating a structure to store cities
 struct city
 {
-    char cities[20]; 
+    char cities[20];
 }city_list[100];
 //creating a structure to store user details
 struct userdetails
@@ -28,15 +28,25 @@ struct userdetails
     char email[100];
     char password[100];
 }user[50];
+//creating a structure to store the details of cheapest flight
+struct cheapestflight
+{
+    float fare;
+    char source[20];
+    char stop[20];
+    char destination[20];
+    char flightID1[10];
+    char flightID2[10];
+}cheapflight[10];
 //Creating prototypes for function
 void files();
 void userdata();
 void userlogin();
 void createnewaccount();
-void login();  
+void login();
 void displayallflights();
-void displaysource();    
-void displaysourcetodesination();   
+void displaysource();
+void displaysourcetodesination();
 int choice();
 //Main function
 int main()
@@ -70,18 +80,18 @@ int choice()
         switch(choice)
         {
             case 1:displayallflights();
-                   break; 
-            case 2:displaysource();       
-                   break; 
-            case 3:displaysourcetodesination(); 
-                   break;      
+                   break;
+            case 2:displaysource();
+                   break;
+            case 3:displaysourcetodesination();
+                   break;
             case 4:printf("Succefully logged out... :)\n");
-            return 0;       
+            return 0;
             default:printf("Invalid choice\n");
-                    break;    
+                    break;
         }
     } while (choice!=4);
-    return 0;   
+    return 0;
 }
 void files()
 {
@@ -89,14 +99,14 @@ void files()
     int i=0;
     //opening FLIGHT DETAILS csv file
     fp = fopen("FLIGHTDETAILS.csv", "r");
-    if (fp == NULL) 
+    if (fp == NULL)
     {
         printf("Error opening file FLIGHT DETAILS");
         return ;
     }
     //opening LOCATION csv file
     fp2 = fopen("PLACES.csv", "r");
-    if (fp2 == NULL) 
+    if (fp2 == NULL)
     {
         printf("Error opening file PLACES");
         return ;
@@ -105,7 +115,7 @@ void files()
     char line1[50];
     while (fgets(line1, sizeof(line1), fp))
     {
-        if(sscanf(line1, "%[^,],%[^,],%[^,],%[^,],%[^,],%f,%f", flights[i].flightID, flights[i].source, flights[i].arrivalTime, flights[i].destination,flights[i].departureTime,&flights[i].flightTime,&flights[i].fare) == 7) 
+        if(sscanf(line1, "%[^,],%[^,],%[^,],%[^,],%[^,],%f,%f", flights[i].flightID, flights[i].source, flights[i].arrivalTime, flights[i].destination,flights[i].departureTime,&flights[i].flightTime,&flights[i].fare) == 7)
         {
             i++;
             countflight++;
@@ -114,28 +124,24 @@ void files()
     i=0;
     char line2[50];
     //reading city details from csv file
-     while (fscanf(fp2, "%s", city_list[i].cities) == 1) 
+     while (fscanf(fp2, "%s", city_list[i].cities) == 1)
     {
         i++;
         countplaces++;
-        if (i == 7) 
-        {
-            break;
-        }
-    } 
+    }
     //closing files
     fclose(fp);
     fclose(fp2);
 }
 void userdata()
 {
-    FILE *fp3;   
+    FILE *fp3;
     fp3 = fopen("Userdetails.csv", "r");
-    if (fp3 == NULL) 
+    if (fp3 == NULL)
     {
         printf("Error opening user files");
         return ;
-    } 
+    }
     int i=0;
     //reading user details
     char line3[50];
@@ -165,11 +171,11 @@ void userlogin()
     do
     {
             scanf(" %s",&option);
-            if (option == 'y' || option == 'Y') 
+            if (option == 'y' || option == 'Y')
             {
-                //if yes asking user to login 
-                login();    
-                return;               
+                //if yes asking user to login
+                login();
+                return;
             }
             else if(option == 'n'|| option == 'N')
             {
@@ -181,8 +187,8 @@ void userlogin()
             {
                 //asking user to enter the option if chose any option rather than y or n
                 printf("\nERROR: Please type [y/Y] or [n/N]: ");
-            }    
-    }while(1);     
+            }
+    }while(1);
     for(int j=0;j<60;j++)
     {
         printf("-");
@@ -190,9 +196,9 @@ void userlogin()
     printf("\n");
 }
 //function for user to create a new account
-void createnewaccount() 
+void createnewaccount()
 {
-    if (countuser>50) 
+    if (countuser>50)
     {
         printf("Maximum number of users reached.\n");
         return;
@@ -203,23 +209,23 @@ void createnewaccount()
         printf("-");
     }
     printf("\n\n%10s","Lets create your new account :)\n");
-    do 
+    do
     {
         name1:
         //asking user to enter their new username
         printf("Enter Your Username: ");
-        scanf("%49s", username);
+        scanf(" %[^\n]", username);
         getchar();
         // Asking user to keep username more than 3 characters
-        if (strlen(username) < 3) 
+        if (strlen(username) < 3|| strchr(username,' ')!=NULL)
         {
-            printf("Username must be at least 3 characters long.\n");
+            printf("Username must be at least 3 characters long and no spaces are allowed\n");
             continue;
         }
-        // Checking for duplicate usernames 
-        for (int i = 0; i < countuser; i++) 
+        // Checking for duplicate usernames
+        for (int i = 0; i < countuser; i++)
         {
-            if (strcmp(username, user[i].username) == 0) 
+            if (strcmp(username, user[i].username) == 0)
             {
                 printf("Username already exists. Please choose a different username.\n");
                 goto name1;
@@ -228,21 +234,21 @@ void createnewaccount()
         // If no issues found break out of the loop
         break;
     } while (1);
-    do 
+    do
     {
         email1:
         printf("Enter your Email Address: ");
-        scanf("%49s", email);
+        scanf(" %49s", email);
         getchar();
         // email validation
-        if (strchr(email, '@') == NULL) 
+        if (strchr(email, '@') == NULL)
         {
             printf("Invalid email address. Please enter a valid email address.\n");
             continue;
         }
-        for (int i = 0; i < countuser; i++) 
+        for (int i = 0; i < countuser; i++)
         {
-            if (strcmp(email, user[i].email) == 0) 
+            if (strcmp(email, user[i].email) == 0)
             {
                 printf("Email already exists. Please choose a different email.\n");
                 goto email1;
@@ -250,13 +256,13 @@ void createnewaccount()
         }
         break;
     } while (1);
-    do 
+    do
     {
         printf("Enter your password: ");
-        scanf("%49s", password);
+        scanf(" %49[^\n]", password);
         getchar();
         // Basic password validation (you can add more checks)
-        if (strlen(password) < 8) 
+        if (strlen(password) < 8)
         {
             printf("Password must be at least 8 characters long.\n");
             continue;
@@ -269,13 +275,13 @@ void createnewaccount()
     strcpy(user[countuser].password, password);
     countuser++;
     printf("\nAccount created successfully!\n");
-    FILE *fp;   
+    FILE *fp;
     fp = fopen("Userdetails.csv", "a");
-    if (fp == NULL) 
+    if (fp == NULL)
     {
         printf("Error opening user files");
         return ;
-    } 
+    }
     fprintf(fp,"%s,%s,%s\n", username, email, password);
     for(int j=0;j<60;j++)
     {
@@ -287,78 +293,78 @@ void createnewaccount()
     printf("%10s","How may i help you\n");
 }
 //function for user to login
-void login() 
+void login()
 {
-    if (countuser == 0) 
+    if (countuser == 0)
     {
         printf("No user data available. Please create a new account.\n");
         createnewaccount();
         return;
     }
-    char name[100], pass[100], option;
-    int error = 0; 
+    char name[100], password[100], option;
+    int error = 0;
     name2:
     printf("Enter your username or email address:\n");
     scanf("%s", name);
     if (error == 2) {
-        printf("Invalid login\n Maximum attempts reached.\n"); 
-        do 
+        printf("Invalid login\n Maximum attempts reached.\n");
+        do
         {
             printf("Do You want to create account? [Y/N] ");
-            scanf(" %s", &option); 
+            scanf(" %s", &option);
             if (option == 'y' || option == 'Y') {
                 createnewaccount();
                 return;
             } else if (option == 'n' || option == 'N') {
                 printf("Sorry could not verify.\n");
                 exit(0);
-            } else 
+            } else
             {
-                printf("\nERROR: Please type [y/Y] or [n/N]: \n");           
+                printf("\nERROR: Please type [y/Y] or [n/N]: \n");
             }
         } while (1);
     }
-    for (int i = 0; i < countuser; i++) 
+    for (int i = 0; i < countuser; i++)
     {
-        if (strcmp(name, user[i].username) == 0 || strcmp(name, user[i].email) == 0) 
-        {               
+        if (strcmp(name, user[i].username) == 0 || strcmp(name, user[i].email) == 0)
+        {
             // Reset error count for each user
-            error = 0; 
+            error = 0;
             while (fgetc(stdin) != '\n' && !feof(stdin));
             password1:
             printf("Enter your password:\n");
-            if(fgets(pass,sizeof(pass),stdin)==NULL)
+            if(fgets(password,sizeof(password),stdin)==NULL)
             {
                 printf("Error reading password\n");
                 exit(0);
             }
-            if (strcasecmp(pass, user[i].password) == 0) 
+            if (strcasecmp(password, user[i].password) == 0)
             {
                 printf("Welcome Back!! %s\n", user[i].username);
                 return;
-            } 
-            else 
+            }
+            else
             {
                 printf("Wrong password!! Please try again\n");
                 error++;
-                if (error == 3) 
+                if (error == 3)
                 {
-                    printf("Invalid login\nMaximun number of attempts reached\n");                                   
-                    do 
+                    printf("Invalid login\nMaximun number of attempts reached\n");
+                    do
                     {
                         printf("Do You want to create account? [Y/N] ");
-                        scanf(" %s", &option); 
-                        if (option == 'y' || option == 'Y') 
+                        scanf(" %s", &option);
+                        if (option == 'y' || option == 'Y')
                         {
                             createnewaccount();
                             return;
-                        } 
-                        else if (option == 'n' || option == 'N') 
+                        }
+                        else if (option == 'n' || option == 'N')
                         {
                             printf("Sorry could not verify.\n");
                             exit(0);
                         }
-                        else 
+                        else
                         {
                             printf("\nERROR: Please type [y/Y] or [n/N]: ");
                         }
@@ -385,7 +391,7 @@ void displayallflights()
         printf("-");
     }
     printf("\n");
-    for (int i = 0; i < countflight; i++) 
+    for (int i = 0; i < countflight; i++)
     {
         printf("%-10s %-10s %-10s %-10s %-10s %-10.2f  %-10.2f\n",
                flights[i].flightID, flights[i].source, flights[i].arrivalTime,
@@ -407,7 +413,7 @@ void displaysource()
     for(int i=0;i<countplaces;i++)
     {
         printf("%d.%s\n",i+1,city_list[i].cities);
-    }   
+    }
     //To verify user is giving option in intergers
     while((scanf("%d",&option)!=1))
     {
@@ -438,17 +444,17 @@ void displaysource()
                     flights[i].flightID, flights[i].source, flights[i].arrivalTime,
                     flights[i].destination, flights[i].departureTime, flights[i].flightTime, flights[i].fare);
         }
-    }           
+    }
     for(int j=0;j<75;j++)
     {
         printf("-");
     }
         printf("\n");
-}    
+}
 //function to display flights from user given source and destination
 void displaysourcetodesination()
 {
-    int option,option1,verify=0;
+    int option,option1,verify=0,cheap=0,temp=0;
     float totaltime,totalfare;
     //Asking user to select the source
     jump_here2:
@@ -456,7 +462,7 @@ void displaysourcetodesination()
     for(int i=0;i<countplaces;i++)
     {
         printf("%d.%s\n",i+1,city_list[i].cities);
-    }   
+    }
     //To verify user is giving option in intergers
     while((scanf("%d",&option)!=1)||option>countplaces)
     {
@@ -468,24 +474,24 @@ void displaysourcetodesination()
     printf("%s\n",city_list[option-1].cities);
     //Asking user to select destination
     printf("Select the destination\n");
-    for(int i=0;i<7;i++)
+    for(int i=0;i<countplaces;i++)
     {
         if(strcmp(city_list[option-1].cities,city_list[i].cities)==0)
         {
             continue;
         }
-        printf("%d.%s\n",i+1,city_list[i].cities);   
-    }   
+        printf("%d.%s\n",i+1,city_list[i].cities);
+    }
     //To verify user is giving option in intergers
     while((scanf("%d",&option1)!=1)||option1>countflight)
     {
         while(getchar()!='\n');
         {
             printf("Error in option ,Please type respective option.\n");
-        }    
+        }
     }
     printf("%s\n",city_list[option1-1].cities);
-    //Asking user to select different destination 
+    //Asking user to select different destination
     if(city_list[option-1].cities==city_list[option1-1].cities||option1>=8||option>=8)
     {
         printf("Please Enter correct source and destination\n");
@@ -509,12 +515,12 @@ void displaysourcetodesination()
                     flights[i].flightID, flights[i].source, flights[i].arrivalTime,
                     flights[i].destination, flights[i].departureTime, flights[i].flightTime, flights[i].fare);
         }
-    }   
+    }
     //Displaying no direct files if not available
     if(verify==0)
     {
         printf("\t\t\tNO DIRECT FLIGHTS ):\n");
-    }        
+    }
     for(int j=0;j<75;j++)
     {
         printf("-");
@@ -543,27 +549,55 @@ void displaysourcetodesination()
             printf("%s","\n**FLIGHT 2**\n\n");
             printf("%-10s %-10s %-10s %-10s %-10s %-10.2f %-10.2f \n",
                     flights[j].flightID, flights[j].source, flights[j].arrivalTime,
-                    flights[j].destination, flights[j].departureTime, flights[j].flightTime, flights[j].fare);        
-            totalfare=flights[i].fare+flights[j].fare;      
-            totaltime=flights[i].flightTime+flights[j].flightTime;  
+                    flights[j].destination, flights[j].departureTime, flights[j].flightTime, flights[j].fare);
+            totalfare=flights[i].fare+flights[j].fare;
+            totaltime=flights[i].flightTime+flights[j].flightTime;
             printf("\n");
             printf("TOTAL TIME = %0.2f hours\n",totaltime);
             printf("TOTAL FARE = Rs. %0.2f\n",totalfare);
+            //storing the details of indirect flights
+            cheapflight[cheap].fare=totalfare;
+            strcpy(cheapflight[cheap].source,flights[i].source);
+            strcpy(cheapflight[cheap].stop,flights[i].destination);
+            strcpy(cheapflight[cheap].destination,flights[j].destination);
+            strcpy(cheapflight[cheap].flightID1,flights[i].flightID);
+            strcpy(cheapflight[cheap].flightID2,flights[j].flightID);
+            cheap++;
             for(int j=0;j<75;j++)
             {
                 printf("-");
             }
         printf("\n");
         }
-    }     
+    }
     //Displaying no Indirect files if not available
     if(verify==0)
     {
         printf("\t\t\tNO INDIRECT FLIGHTS ):\n");
+    }
+    //if indirect flights are available displaying details of cheapest flight
+    else
+    {
+        for(int i=0;i<cheap-1;i++)
+        {
+            if(cheapflight[temp].fare>cheapflight[temp+1].fare)
+            {
+                temp=temp+1;
+            }
+        }
+        printf("\n%40s\n","CHEAPEST INDIRECT FLIGHT");
         for(int j=0;j<75;j++)
         {
             printf("-");
         }
-    }      
+        printf("\n");
+        printf("\nFLIGHTID'S: %s, %s\n",cheapflight[temp].flightID1,cheapflight[temp].flightID2);
+        printf("\nCHECKPOINTS: %s->%4s->%4s\n",cheapflight[temp].source,cheapflight[temp].stop,cheapflight[temp].destination);
+        printf("\nFARE = Rs. %0.2f\n",cheapflight[temp].fare);
+    }
+    for(int j=0;j<75;j++)
+    {
+        printf("-");
+    }
     printf("\n");
 }
